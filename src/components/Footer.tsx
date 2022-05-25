71bb53b915a92450c37b1757c6c5bd27d5dc1784
Florian Hye <florian@hye.dev>
2022-05-25 22:44:18 +0200

    added About link to GitHub Readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,21 @@
 import Link from "next/link";
 import React from "react";
-import { FaGithub } from "react-icons/fa";
+import { FaGithub, FaInfoCircle } from "react-icons/fa";
 
 const Footer = ({ className }: { className?: string }) => {
   return (
-    <div className={`mb-4 text-black ${className ? className : ""}`}>
+    <div className={`flex gap-8 mb-4 text-black ${className ? className : ""}`}>
       <Link href={"https://github.com/Flo2410/noten-schnitt"}>
         <span className="flex items-center gap-2 cursor-pointer">
           <FaGithub />
           Check out the Source
+        </span>
+      </Link>
+
+      <Link href={"https://github.com/Flo2410/noten-schnitt/blob/main/README.md"}>
+        <span className="flex items-center gap-2 cursor-pointer">
+          <FaInfoCircle />
+          About
         </span>
       </Link>
     </div>
