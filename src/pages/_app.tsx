95ae039f9f82258f5904f2f3626630dd2efcf2c4
Florian Hye <florian@hye.dev>
2022-05-25 18:39:27 +0200

    fixed some colors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,11 +11,13 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#3B82F6" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#da532c" />
+
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#3B82F6" />
+        <meta name="theme-color" content="#E6E7EB" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <UserProvider>
         <Component {...pageProps} />
