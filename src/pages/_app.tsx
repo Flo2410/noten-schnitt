51c5b896c23ceed15ade20fe9be6fb9dc32cfdfc
Florian Hye <florian@hye.dev>
2022-07-16 20:24:42 +0200

    fixed pwa titlebar color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,12 +15,12 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#3B82F6" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#163072" />
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
 
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#3B82F6" />
-        <meta name="theme-color" content="#E6E7EB" />
+        <meta name="msapplication-TileColor" content="#163072" />
+        <meta name="theme-color" content="#163072" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <SettingsProvider>
