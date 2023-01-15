80651c19a32ec9e6c7c7aed47302ea629acdb4d2
Flo2410 <florian@hye.dev>
2023-01-15 14:54:14 +0100

    made getCookiesAsString generic

--- src/helper/utils.ts
+++ src/helper/utils.ts
@@ -1,3 +1,9 @@
 import { UserCookies } from "types/user.types";
+import { UserCookies as UserCookiesV2 } from "types/user-v2.types";
 
-export const getCookiesAsString = (cookies: UserCookies) => cookies.fhwn + "; " + cookies.session;
+// export const getCookiesAsString = (cookies: UserCookies) => cookies.fhwn + "; " + cookies.session;
+// export const getCookiesAsStringV2 = (cookies: UserCookiesV2) =>
+//   cookies.culture + "; " + cookies.asp_net_core;
+
+export const getCookiesAsString = (cookies: UserCookies | UserCookiesV2) =>
+  Object.values(cookies).join(";");
