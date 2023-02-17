e59407123a1dd9a3ebbbe533e231a544c290b42c
Flo2410 <florian@hye.dev>
2023-02-17 18:16:28 +0100

    /v2/noten now working

--- src/pages/api/v2/noten.ts
+++ src/pages/api/v2/noten.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as cheerio from "cheerio";
+import { v4 as uuidv4 } from "uuid";
 import { getCookiesAsString } from "helper/utils";
 import { log } from "helper/logger";
-
+import { Note } from "types/noten.types";
+import { UserCookies } from "types/user-v2.types";
 // const fetch = fetchCookie(nodeFetch);
 
 export default async function handler(
@@ -12,13 +14,55 @@ export default async function handler(
 ) {
   const start_time = Date.now();
 
+  const dates = await getSemsterDates(req.body.cookies);
+
+  const promises = dates.map((date) =>
+    getNotenForSemester(req.body.cookies, req.body.noten_nummer, date)
+  );
+
+  const noten = await (await Promise.all(promises)).flat();
+
+  res.status(200).send(noten);
+  log("info", req.method, req.url, 200, start_time, Date.now());
+}
+
+const getSemsterDates = async (cookies: UserCookies) => {
+  const data = await fetch(`https://cis.fhwn.ac.at/Grades/StudentGradesOverview/Index`, {
+    method: "GET",
+    headers: {
+      "User-Agent": "Mozilla/5.0",
+      Cookie: getCookiesAsString(cookies),
+    },
+  });
+
+  // Check if request was redirected to login page
+  if (data.url.includes("https://cis.fhwn.ac.at/Home/Index")) {
+    throw new Error("Cookie is not valid!");
+  }
+
+  let html = await data.text();
+  html = html.replaceAll("\t", "").replaceAll("\r", "").replaceAll("\n", "");
+  const $ = cheerio.load(html, null, false);
+
+  const dates = $("#selSemester > option")
+    .toArray()
+    .map((item) => item.attributes[0].value)
+    .filter((item) => item !== "placeholder");
+  return dates;
+};
+
+const getNotenForSemester = async (
+  cookies: UserCookies,
+  noten_nummer: string,
+  semester_date: string
+) => {
   const data = await fetch(
-    `https://cis.fhwn.ac.at/Grades/StudentGradesOverview/GradesList?bisdatum=15.11.2022&selectedPkzFromStudent=${req.body.pers_nummer}`,
+    `https://cis.fhwn.ac.at/Grades/StudentGradesOverview/GradesList?bisdatum=${semester_date}&selectedPkzFromStudent=${noten_nummer}`,
     {
       method: "GET",
       headers: {
         "User-Agent": "Mozilla/5.0",
-        Cookie: getCookiesAsString(req.body.cookies),
+        Cookie: getCookiesAsString(cookies),
       },
     }
   );
@@ -30,73 +74,45 @@ export default async function handler(
 
   const json = await data.json();
   let html = json.Result as string;
-  html = html.replaceAll("\t", "").replaceAll("\r", "");
+  html = html.replaceAll("\t", "").replaceAll("\r", "").replaceAll("\n", "");
 
   const $ = cheerio.load(html, null, false);
 
-  const tbody = $("#collapse5 > div > table > tbody");
-  const noten = tbody.map(function (i, el) {
-    return $(this)
-      .text()
-      .trim()
-      .split("\n")
-      .filter((value) => value.length > 0);
+  const tbody = $("div.viewDesktop > div.accordion > div > div > div > table > tbody");
+
+  const semester = $("h4.smallerText").first().text().match(/\d/g)?.[0] ?? "";
+
+  const trs = tbody
+    .toString()
+    .replace("<tbody>", "")
+    .replace("</tbody>", "")
+    .replaceAll("<tr>", "")
+    .split("</tr>")
+    .filter((item) => item);
+
+  const noten = trs.map((tr) => {
+    const arr = tr
+      .replaceAll("<td>", "")
+      .split("</td>")
+      .filter((item) => item);
+
+    const art = arr[0].match(/(?<=\()(.*?)(?=\))/g)?.[0] ?? "";
+    const lv = arr[0].replace(`(${art})`, "").trim();
+
+    let note: Note = {
+      internal_id: uuidv4(),
+      note: arr[1].match(/\d/g)?.[0] ?? "",
+      art: art,
+      lv: lv,
+      ects: "",
+      date: semester_date,
+      semester: semester,
+      exlude: false,
+      perm_exlude: false,
+    };
+
+    return note;
   });
 
-  // const noten: Array<Note> = [];
-  // let count = 0;
-  // let note: Note = {
-  //   internal_id: uuidv4(),
-  //   note: "",
-  //   art: "",
-  //   lv: "",
-  //   ects: "",
-  //   date: "",
-  //   semester: "",
-  //   exlude: false,
-  //   perm_exlude: false,
-  // };
-  // const valid_noten = ["1", "2", "3", "4"];
-
-  // arr.forEach((item) => {
-  //   switch (count) {
-  //     case 0:
-  //       note.note = item;
-  //       note.perm_exlude = !valid_noten.includes(note.note);
-  //       break;
-  //     case 1:
-  //       note.art = item;
-  //       break;
-  //     case 2:
-  //       note.lv = item;
-  //       break;
-  //     case 3:
-  //       note.ects = item;
-  //       break;
-  //     case 4:
-  //       note.date = item;
-  //       break;
-  //     case 5:
-  //       note.semester = item;
-  //       noten.push(note);
-  //       note = {
-  //         internal_id: uuidv4(),
-  //         note: "",
-  //         art: "",
-  //         lv: "",
-  //         ects: "",
-  //         date: "",
-  //         semester: "",
-  //         exlude: false,
-  //         perm_exlude: false,
-  //       };
-  //       break;
-  //   }
-  //   count++;
-  //   if (count >= 6) count = 0;
-  // });
-
-  // noten.splice(0, 1);
-  res.status(200).send(noten);
-  log("info", req.method, req.url, 200, start_time, Date.now());
-}
+  return noten;
+};
