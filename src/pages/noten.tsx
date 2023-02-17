621cf99b3c44607f30015b87c38097f0b2961046
Flo2410 <florian@hye.dev>
2023-02-17 19:14:48 +0100

    UserHeader showing info from user v2

--- src/pages/noten.tsx
+++ src/pages/noten.tsx
@@ -8,11 +8,13 @@ import UserHeader from "components/UserHeader";
 import { UserContext } from "context/UserContext";
 import Router from "next/router";
 import React, { useContext, useEffect, useState } from "react";
+import { useUserStore } from "stores/userStore_v2";
 import { USER_COOKIE_KEY } from "types/user.types";
 
 const NotenPage = () => {
   const { state: user, logout, isLoading } = useContext(UserContext);
   const [show_excluded, setShowExcluded] = useState(true);
+  const user_v2 = useUserStore((state) => state.user);
 
   useEffect(() => {
     if (!localStorage.getItem(USER_COOKIE_KEY)) Router.push("/login");
@@ -21,7 +23,7 @@ const NotenPage = () => {
   return (
     <>
       <div className="flex flex-col items-center min-h-screen px-2 md:px-0 pwa:px-2 body-setup">
-        <UserHeader user={user} onLogout={() => logout()} />
+        <UserHeader user={user_v2} onLogout={() => logout()} />
         <NotenSchnitt noten={user.noten} />
         <Options show_excluded={show_excluded} setShowExcluded={setShowExcluded} />
         <NotenListe show_excluded={show_excluded} />
