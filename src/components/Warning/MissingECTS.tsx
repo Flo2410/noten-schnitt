3875af0177fa15afd7c54108d5bec7fc00af928f
Flo2410 <florian@hye.dev>
2023-02-19 17:24:08 +0100

    fixed layout in ects info

--- src/components/Warning/MissingECTS.tsx
+++ src/components/Warning/MissingECTS.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const MissingECTS = () => {
   return (
     <div className="flex justify-center w-full mt-4">
-      <div className="grid w-full grid-cols-1 px-4 py-2 shadow-fhwn 2xl:w-1/2 md:grid-cols-2 sm:flex-row md:w-2/3 pwa:w-full dark:border">
+      <div className="flex flex-col w-full px-4 py-2 shadow-fhwn 2xl:w-1/2 md:w-2/3 pwa:w-full dark:border">
         <span className="text-2xl font-bold text-center text-red-500">HINWEIS! </span>
         <p className="text-lg text-center">
           Im Notenschnitt werden keine Noten einberrechnet, welche aus dem CIS geladen wurden.
