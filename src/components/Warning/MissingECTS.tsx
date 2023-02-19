d8633e618bbf9a00069f52cb1e48ccf7ecf696c9
Flo2410 <florian@hye.dev>
2023-02-19 19:35:06 +0100

    fix typo

--- src/components/Warning/MissingECTS.tsx
+++ src/components/Warning/MissingECTS.tsx
@@ -6,8 +6,8 @@ export const MissingECTS = () => {
       <div className="flex flex-col w-full px-4 py-2 shadow-fhwn 2xl:w-1/2 md:w-2/3 pwa:w-full dark:border">
         <span className="text-2xl font-bold text-center text-red-500">HINWEIS! </span>
         <p className="text-lg text-center">
-          Im Notenschnitt werden keine Noten einberrechnet, welche aus dem CIS geladen wurden.
-          Leider werden im CIS keine ECTS angegeben.
+          Im Notenschnitt werden keine Noten einberechnet, welche aus dem CIS geladen wurden. Leider
+          werden im CIS keine ECTS angegeben.
         </p>
       </div>
     </div>
