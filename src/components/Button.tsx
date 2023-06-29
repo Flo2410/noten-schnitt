0ffea9c07dc54484cb766e9b19a83cb804aebb2e
Flo2410 <florian@hye.dev>
2023-06-29 18:27:54 +0200

    made the Button components colors invertable

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
 import React, { ButtonHTMLAttributes, FC } from "react";
 
-const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = ({ children, className, ...props }) => {
+const Button: FC<ButtonHTMLAttributes<HTMLButtonElement> & { inverted_colors?: boolean }> = ({
+  children,
+  className,
+  inverted_colors = false,
+  ...props
+}) => {
   return (
     <button
-      className={`flex justify-center h-8 px-4 text-center items-center text-white border-2 rounded-md shadow-fhwn bg-primary hover:bg-white hover:text-primary border-primary dark:bg-white dark:border-white dark:text-primary dark:hover:text-white dark:hover:bg-primary ${
-        className ? className : ""
-      }`}
+      className={`flex justify-center h-8 px-4 text-center items-center border-2 rounded shadow-fhwn 
+      ${
+        inverted_colors
+          ? "text-primary bg-white hover:bg-primary hover:text-white border-white dark:bg-primary dark:border-primary dark:text-white dark:hover:text-primary dark:hover:bg-white"
+          : "text-white bg-primary hover:bg-white hover:text-primary border-primary dark:bg-white dark:border-white dark:text-primary dark:hover:text-white dark:hover:bg-primary"
+      } ${className ? className : ""}`}
       {...props}
     >
       {children}
