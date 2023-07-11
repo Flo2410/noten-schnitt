97b701fcc025cc41ffdc200121b9d2804826ef84
Flo2410 <florian@hye.dev>
2023-07-11 19:47:06 +0200

    add breakpoint tailwindcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,40 @@ module.exports = {
       },
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    ({ addBase, theme }) => {
+      if (process.env.NODE_ENV === "production") return;
+
+      const screens = theme("screens", {});
+      const breakpoints = Object.keys(screens);
+
+      addBase({
+        "body::after": {
+          content: `"Current breakpoint default (< ${screens[breakpoints[0]]})"`,
+          position: "fixed",
+          right: ".5rem", // could replace with theme('spacing.2', '.5rem'), same for most of the other values
+          bottom: ".5rem",
+          padding: ".5rem .5rem .5rem 2rem",
+          background:
+            "no-repeat .5rem center / 1.25rem url(https://icons-for-free.com/iconfiles/png/512/vscode+icons+type+tailwind-1324451500323172563.png), #edf2f7",
+          border: "1px solid #cbd5e0",
+          color: "#d53f8c",
+          fontSize: ".875rem",
+          fontWeight: "600",
+          zIndex: "99999",
+        },
+        ...breakpoints.reduce((acc, current) => {
+          acc[`@media (min-width: ${screens[current]})`] = {
+            "body::after": {
+              content: `"Current breakpoint ${current}"`,
+            },
+          };
+          return acc;
+        }, {}),
+      });
+    },
+  ],
   daisyui: {
     themes: ["light", "dark"],
     styled: true,
